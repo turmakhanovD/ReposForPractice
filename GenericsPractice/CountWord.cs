b975c9c4ed22b13a3c0d5e0e67efb130b719b440
Турмахан Динислам Даулетулы <@corp.itstep>
2019-03-14 17:28:03 +0600

    first

---
+++ GenericsPractice/CountWord.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsPractice
+{
+    public class CountWord
+    {
+        public Dictionary<string, int> wordList = new Dictionary<string, int>();
+
+        public void AddWord(string word)
+        {
+            if (wordList.ContainsKey(word))
+            {
+                wordList[word] += 1;
+            }
+            else
+            {
+                wordList.Add(word, 1);
+            }
+        }
+
+        public void Stats()
+        {
+            int cnt = 1;
+            int sum = 0;
+            int unqSum = 0;
+            foreach (var i in wordList)
+            {
+                if(i.Key.Length>5)
+                Console.WriteLine( "{2}.\t {0} \t{1} ", i.Key, i.Value,cnt);
+                else
+                    Console.WriteLine("{2}.\t {0}\t\t{1} ", i.Key, i.Value, cnt);
+                cnt++;
+                sum += i.Value;
+                if (i.Value == 1) unqSum += 1;
+            }
+            Console.WriteLine("\nОбщее количество слов: {0} из них уникальных: {1}",sum,unqSum);
+
+        }
+    }
+}
---
+++ GenericsPractice/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace GenericsPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountWord countWord = new CountWord();
+            string text = "Вот дом, Который построил Джек. А это пшеница, Кото­рая в темном  чулане хранится В доме, Который построил Джек. А это веселая птица­ синица, Которая часто вору­ет пшеницу, Которая в темном чулане хранится В доме, Который построил Джек";
+            char[] separator = { ' ', ',', '.','!' };
+            string[] words = text.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+        
+            foreach (string i in words)
+            {
+                countWord.AddWord(i);
+            }
+            StringFormat stringFormat = new StringFormat();
+         
+ 
+            Console.WriteLine("\t Слово:\t\tКол-во:");
+           
+            countWord.Stats();
+            Console.ReadLine();
+        }
+    }
+}
